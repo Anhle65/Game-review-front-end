f11ff9ad1cc878672d06334cbb364a3bc80faa68
Anhle65 <levananh235813@gmail.com>
2025-04-29 22:02:38 +1200

    create store

---
+++ game-review/src/store/index.ts
@@ -0,0 +1,35 @@
+import {create} from "zustand/react";
+import {use} from "react";
+
+interface GameState {
+    games: Game[];
+    setGames: (games: Array<Game>) => void;
+    editGame: (game: Game, newGamename: string) => void;
+    removeGame: (game: Game) => void;
+}
+const getlocalStorage = (key: string): Array<Game> => JSON.parse(window.localStorage.getItem(key) as string);
+const setLocalStorage = (key: string, value: Array<Game>) => window.localStorage.setItem(key, JSON.stringify(value));
+const useStore = create<GameState>((set) => ({
+    games: getlocalStorage('games') || [],
+    setGames: (games: Array<Game>) => set(() => {
+        setLocalStorage('games', games)
+        return {games: games}
+    }),
+    editGame: (game: Game, newGamename) => set((state) => {
+        const temp = state.games.map(u=>u.gameId === game.gameId ?
+            ({...u, title: newGamename} as Game): u)
+        setLocalStorage('games', temp)
+        return {
+            games: state.games.map(u => u.gameId === game.gameId ?
+                ({ ...u, title: newGamename} as Game) : u)
+        }
+    }),
+    removeGame: (game: Game) => set((state) => {
+        setLocalStorage('games', state.games.filter(u=>u.gameId !== game.gameId))
+        return {
+            games: state.games.filter(u=>u.gameId !== game.gameId)
+        }
+    })
+}))
+
+export const useGameStore = useStore;
