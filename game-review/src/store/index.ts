da4079481c430de03a747273df7ea7a832216095
Anhle65 <levananh235813@gmail.com>
2025-04-30 23:22:32 +1200

    add genres in the GameState and also the localStorage

--- game-review/src/store/index.ts
+++ game-review/src/store/index.ts
@@ -3,17 +3,30 @@ import {use} from "react";
 
 interface GameState {
     games: Game[];
+    genres: Genre[];
     setGames: (games: Array<Game>) => void;
+    setGenres: (genres: Array<Genre>) => void;
     editGame: (game: Game, newGamename: string) => void;
     removeGame: (game: Game) => void;
 }
-const getlocalStorage = (key: string): Array<Game> => JSON.parse(window.localStorage.getItem(key) as string);
-const setLocalStorage = (key: string, value: Array<Game>) => window.localStorage.setItem(key, JSON.stringify(value));
+// const getlocalStorage = (key: string): Array<Game> => JSON.parse(window.localStorage.getItem(key) as string);
+// const setLocalStorage = (key: string, value: Array<Game>) => window.localStorage.setItem(key, JSON.stringify(value));
+const getLocalStorage = <T>(key: string): T[] =>
+    JSON.parse(window.localStorage.getItem(key) || '[]');
+
+const setLocalStorage = <T>(key: string, value: T[]) =>
+    window.localStorage.setItem(key, JSON.stringify(value));
+
 const useStore = create<GameState>((set) => ({
-    games: getlocalStorage('games') || [],
+    games: getLocalStorage<Game>('games') || [],
+    genres: getLocalStorage<Genre>('genres') || [],
     setGames: (games: Array<Game>) => set(() => {
         setLocalStorage('games', games)
         return {games: games}
+    }),
+    setGenres: (genres: Array<Genre>) => set(() => {
+        setLocalStorage('genres', genres)
+        return {genres: genres}
     }),
     editGame: (game: Game, newGamename) => set((state) => {
         const temp = state.games.map((g: Game) =>g.gameId === game.gameId ?
