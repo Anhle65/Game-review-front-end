d421168967fc14c530ced14fdca0af1475179cb2
Anhle65 <levananh235813@gmail.com>
2025-04-30 23:24:33 +1200

    get all genres{id, name} from endpoint and pass into GameListObject props

--- game-review/src/components/GameList.tsx
+++ game-review/src/components/GameList.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import axios from "axios";
 import CSS from 'csstype';
-import {Alert, AlertTitle, Paper} from "@mui/material";
+import {Alert, AlertTitle, Pagination, PaginationItem, Paper, Stack} from "@mui/material";
 import {useGameStore} from "../store";
 import { rootUrl } from "../base.routes";
 import GameListObject from "./GameListObject";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const GameList = () => {
     const games = useGameStore(state => state.games);
+    const genres = useGameStore(state => state.genres);
     const setGames = useGameStore(state => state.setGames);
+    const setGenres = useGameStore(state => state.setGenres);
     const [errorFlag, setErrorFlag] = React.useState(false);
     const [errorMessage, setErrorMessage] = React.useState("");
+    const [currentPage, setCurrentPage] = React.useState(1);
+
     React.useEffect(() => {
             const getGames = () => {
                 axios.get('http://localhost:4941' +rootUrl+'/games')
@@ -26,7 +31,21 @@ const GameList = () => {
             getGames();
         }, [setGames]
     )
-    const user_rows = () => games.map((game: Game) => <GameListObject key={game.gameId + game.title} game={game}/>);
+    React.useEffect(() => {
+        const getGenres = () => {
+            axios.get('http://localhost:4941' +rootUrl+'/games/genres')
+                .then((response) => {
+                    setGenres(response.data)
+                    setErrorFlag(false);
+                    setErrorMessage("");
+                }, (error) => {
+                    setErrorFlag(true);
+                    setErrorMessage(error.toString() + " defaulting to old users changes app may not work as expected")
+                })
+        }
+        getGenres()
+    },[setGenres])
+    const game_rows = () => games.slice((currentPage - 1) * 9, currentPage * 9).map((game: Game) => <GameListObject key={game.gameId + game.title} game={game} genres={genres}/>);
     const card: CSS.Properties = {
         padding: "10px",
         margin: "20px",
@@ -34,19 +53,42 @@ const GameList = () => {
         width: "fit-content"
     }
     return (
-        <Paper elevation={3} style={card} >
-            <h1>UserList </h1>
-            <div style={{ display: "inline-block", maxWidth: "965px", minWidth: "320px" }}>
-                { errorFlag ? (
-                    <Alert severity="error">
-                        <AlertTitle>Error</AlertTitle>
-                        {errorMessage}
-                    </Alert>
-                ) : null }
-                { user_rows() }
-            </div>
-        </Paper>
-    )
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Paper elevation={3} style={card}>
+                <h1>GameList </h1>
+                <div style={{display: "inline-block", maxWidth: "965px", minWidth: "320px"}}>
+                    {errorFlag ? (
+                        <Alert severity="error">
+                            <AlertTitle>Error</AlertTitle>
+                            {errorMessage}
+                        </Alert>
+                    ) : null}
+                    {game_rows()}
+                </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <Pagination
+                        count={Math.ceil(games.length / 9)}
+                        showFirstButton showLastButton
+                        onChange={(event, value) => setCurrentPage(value)}
+                        renderItem={(item) => (
+                            <PaginationItem
+                                slots={{previous: ArrowBackIcon, next: ArrowForwardIcon}}
+                                {...item}
+                            />
+                        )}
+                    />
+                </div>
+            </Paper>
+        </div>
+)
 
 }
 export default GameList;
