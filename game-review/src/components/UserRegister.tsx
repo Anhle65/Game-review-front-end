a9fd1dc174b2ef29432f51c80a21780c75d882db
Anhle65 <levananh235813@gmail.com>
2025-05-03 17:10:09 +1200

    add functional form for new registration

--- game-review/src/components/UserRegister.tsx
+++ game-review/src/components/UserRegister.tsx
@@ -9,6 +9,9 @@ import {Card, CardContent, Stack, Typography} from "@mui/material";
 const UserRegister = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
     const [error, setError] = useState('');
     const [errorFlag, setErrorFlag] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
@@ -23,76 +26,133 @@ const UserRegister = () => {
         setError('');
         setErrorFlag(false);
     }
+
+    const updateFnameState = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFname(event.target.value)
+        setError('');
+        setErrorFlag(false);
+    }
+    const updateLnameState = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLname(event.target.value)
+        setError('');
+        setErrorFlag(false);
+    }
     const updatePasswordState = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(event.target.value)
         setError('');
         setErrorFlag(false);
     }
+    const updateConfirmPasswordState = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setConfirmPassword(event.target.value)
+        setError('');
+        setErrorFlag(false);
+    }
     const onSubmit = async () => {
         try {
-            const response = await
-                axios.post("http://localhost:4941"+rootUrl + '/users/login', {
+            if(password !== confirmPassword) {
+                setErrorFlag(true);
+                setError('Password and confirm password is not matched');
+            } else {
+             await axios.post("http://localhost:4941"+rootUrl + '/users/register', {
+                    firstName: fname,
+                    lastName: lname,
                     email: email,
                     password: password
                 }, {
                     timeout: 10000
                 });
-            setErrorFlag(false);
-            setError('');
-            const {userId, token} = response.data;
-            localStorage.setItem('token', token);
-            localStorage.setItem('userId', userId);
-            console.log(userId);
-            navigate(rootUrl + '/users/' + userId);
+                setErrorFlag(false);
+                setError('');
+                console.log(email);
+                console.log('fname'+fname);
+                console.log('lname'+lname);
+
+                const response = await axios.post("http://localhost:4941"+rootUrl + '/users/login', {
+                    email: email,
+                    password: password
+                }, {
+                    timeout: 10000
+                });
+                const {userId, token} = response.data;
+                localStorage.setItem('token', token);
+                localStorage.setItem('userId', userId);
+                navigate(rootUrl + '/users/' + userId);
+            }
         } catch(error:any) {
             console.log(error);
+            console.log('fname:'+fname);
+            console.log('lname:'+lname);
             console.log(email);
             console.log(password);
             setErrorFlag(true);
             if (axios.isAxiosError(error)) {
                 if (error.response?.status === 400) {
-                    if(password.length < 6 || password.length > 64) {
-                        setError("Password length must be from 6 to 64 characters");
-                    } else
-                        setError("Invalid email");
+                    if(fname.length < 6 || fname.length > 64) {
+                        setError("Fist name length must be from 6 to 64 characters");
+                    } else {
+                        if (lname.length < 6 || lname.length > 64) {
+                            setError("Last name length must be from 6 to 64 characters");
+                        } else {
+                            if(password.length < 6 || password.length > 64) {
+                                setError("Password length must be from 6 to 64 characters");
+                            } else {
+                                setError("Invalid email");
+                            }
+                        }
+                    }
                 } else{
-                    if (error.response?.status === 401) {
-                        setError("Wrong email or password");
+                    if (error.response?.status === 403) {
+                        setError("Email is already used");
                     } else
-                        setError("Failed to login");
+                        setError("Internal Server Error");
                 }
             } else {
                 setError("Unexpected error");
             }
         }
     };
-    const loginCardStyles: CSS.Properties = {
+    const signUpCardStyles: CSS.Properties = {
         display: "inline-block",
-        height: "500px",
+        height: "600px",
         width: "300px",
         margin: "10px",
         padding: "0px",
         backgroundColor: "lightcyan",
     }
     return (
-        <div className="login-form-container">
-            <h2 className="login-title">Login</h2>
+        <div className="signup-form-container">
+            <h2 className="signup-title">Register</h2>
             {error && <Alert variant="danger">{error}</Alert>}
-            <Card sx={loginCardStyles}>
+            <Card sx={signUpCardStyles}>
                 <CardContent >
                     <form>
                         <Stack direction="column" spacing={2} sx={{
                             justifyContent: "space-around",
                             alignItems: "center",
                         }}>
-                            <div className="login-form">
+                            <div className="signup-form">
+                                <label htmlFor="fname">First Name:</label>
+                                <input type="text" className="form-control" id="fname" onChange={updateFnameState}/>
+                            </div>
+                            <div className="signup-form">
+                                <label htmlFor="lname">Last Name:</label>
+                                <input type="text" className="form-control" id="lname" onChange={updateLnameState}/>
+                            </div>
+                            <div className="signup-form">
                                 <label htmlFor="email">Email address:</label>
                                 <input type="email" className="form-control" id="email" onChange={updateEmailState}/>
                             </div>
-                            <div className="login-form">
+                            <div className="signup-form">
                                 <label htmlFor="pwd">Password:</label>
-                                <input type={pwFormType} className="form-control" id="pwd" onChange={updatePasswordState}/>
+                                <input type={pwFormType} className="form-control" id="pwd"
+                                       onChange={updatePasswordState}/>
                             </div>
+                            <div className="signup-form">
+                                <label htmlFor="cfpwd">Confirm password:</label>
+                                <input type={pwFormType} className="form-control" id="cfpwd"
+                                       onChange={updateConfirmPasswordState}/>
+                            </div>
+
                             <div className="checkbox">
                                 <label><input type="checkbox" checked={showPassword}
                                               onChange={() => setShowPassword(prev => !prev)}/> Show password</label>
@@ -100,7 +160,8 @@ const UserRegister = () => {
                             <button type="button" className="btn btn-success" onClick={(e) => {
                                 e.preventDefault(); // Prevent form from refreshing the page
                                 onSubmit();
-                            }}>Success</button>
+                            }}>Create account
+                            </button>
                         </Stack>
                     </form>
                 </CardContent>
