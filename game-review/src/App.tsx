13be68f73fdbb434f679cd271455f61af5caa9d1
Anhle65 <levananh235813@gmail.com>
2025-05-06 23:24:54 +1200

    add a page to see all games that are created by logged-in user

--- game-review/src/App.tsx
+++ game-review/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import NotFound from "./components/NotFound";
-import {rootUrl} from "./base.routes";
 import Game from "./components/Game";
 import Login from "./components/Login";
 import NewGame from "./components/NewGame";
@@ -11,6 +10,7 @@ import UserProfile from "./components/UserProfile";
 
 import GameList from "./components/GameList";
 import EditUserProfile from "./components/EditUserProfile";
+import UserGames from "./components/UserGames";
 function App() {
   return (
     <div className="App">
@@ -24,6 +24,7 @@ function App() {
             <Route path="/games/user/:id" element={<GameList />} />
             <Route path="/users/:id/profile" element={<UserProfile />} />
             <Route path="/users/:id/edit" element={<EditUserProfile />} />
+            <Route path="/users/:id/myGames" element={<UserGames />} />
             <Route path="/users/login" element={<Login />} />
             <Route path="/users/register" element={<UserRegister />} />
             <Route path="*" element={<NotFound />} />
