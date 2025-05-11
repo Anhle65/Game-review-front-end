d3cb1d747b3378ac9ca18ed1df01e5930aeece1f
Anhle65 <levananh235813@gmail.com>
2025-05-11 21:21:58 +1200

    set condition for setting price but still need to refine UI

--- game-review/src/components/NewGame.tsx
+++ game-review/src/components/NewGame.tsx
@@ -34,20 +34,20 @@ const NewGame = () => {
     const authorization = useUserStore();
     const token = authorization.token;
     const navigate = useNavigate();
-    const errorChecked = allPlatforms.filter(p => p.isSelected).length < 1;
+    const platformChecked = allPlatforms.filter(p => p.isSelected).length < 1;
     const [image, setImage] = React.useState('https://png.pngitem.com/pimgs/s/150-1503945_transparent-user-png-default-user-image-png-png.png');
     const [imageFile, setImageFile] = React.useState<File | null>(null);
     const onCreateGame = async () => {
         console.log("Chosen platforms: ", allPlatforms.filter(p => p.isSelected));
         console.log("Chosen platforms id: ", allPlatforms.filter(p => p.isSelected).map(i => i.platformId));
-        // if(!errorChecked) {
+        if(!errorFlag)
             try {
                 const gamePost = await axios.post("http://localhost:4941" + rootUrl + "/games", {
                     title: title,
                     description: description,
                     creationDate: new Date(),
                     genreId: parseInt(genre, 10),
-                    price: parseInt(price, 10)*100,
+                    price: parseInt(price, 10),
                     platformIds: allPlatforms.filter(p => p.isSelected).map(i => parseInt(String(i.platformId), 10))
                 }, {
                     headers: {
@@ -72,39 +72,38 @@ const NewGame = () => {
                     setErrorMessage(error.message.toString());
                 }
             }
-        // } else {
-        //     setErrorFlag(true);
-        // }
     }
     const onUpdateGame = async () => {
-        try{
-            await axios.patch("http://localhost:4941" + rootUrl + "/games/" + id, {
-                gameId: id,
-                title: title,
-                description: description,
-                genreId: parseInt(genre, 10),
-                price: parseInt(price, 10)*100,
-                platformIds: allPlatforms.filter(p => p.isSelected).map(i => parseInt(String(i.platformId), 10))
-            }, {
-                headers: {
-                    "X-Authorization": token
-                }
-            })
-            if(imageFile) {
-                await axios.put("http://localhost:4941" + rootUrl + "/games/" + id + "/image", imageFile, {
+        if(!errorFlag) {
+            try {
+                await axios.patch("http://localhost:4941" + rootUrl + "/games/" + id, {
+                    gameId: id,
+                    title: title,
+                    description: description,
+                    genreId: parseInt(genre, 10),
+                    price: parseInt(price, 10),
+                    platformIds: allPlatforms.filter(p => p.isSelected).map(i => parseInt(String(i.platformId), 10))
+                }, {
                     headers: {
-                        "X-Authorization": token,
-                        "Content-Type": imageFile?.type,
+                        "X-Authorization": token
                     }
                 })
+                if (imageFile) {
+                    await axios.put("http://localhost:4941" + rootUrl + "/games/" + id + "/image", imageFile, {
+                        headers: {
+                            "X-Authorization": token,
+                            "Content-Type": imageFile?.type,
+                        }
+                    })
+                }
+                navigate('/games/' + id);
+            } catch (error) {
+                setErrorFlag(true);
+                if (axios.isAxiosError(error)) {
+                    setErrorMessage(error.message.toString());
+                }
+                setErrorMessage("Unexpected error");
             }
-            navigate('/games/' + id);
-        } catch (error) {
-            setErrorFlag(true);
-            if (axios.isAxiosError(error)) {
-                setErrorMessage(error.message.toString());
-            }
-            setErrorMessage("Unexpected error");
         }
     }
     React.useEffect(() => {
@@ -128,7 +127,7 @@ const NewGame = () => {
             getGameImage();
         } else {
             setTitle("");
-            setDescription("");
+            setDescription(" ");
             setGenre('');
             setPrice('0');
             setAllPlatforms(prevPlatforms =>
@@ -167,11 +166,12 @@ const NewGame = () => {
     }
     const handlePlatformSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, checked } = event.target;
-        setAllPlatforms((prev) =>
-            prev.map((p) =>
+        setAllPlatforms((prev) => {
+            const update = prev.map((p) =>
                 p.name === name ? { ...p, isSelected: checked } : p
             )
-        );
+            return update;
+        });
     };
 
     const getGenres = async () => {
@@ -213,6 +213,12 @@ const NewGame = () => {
     }
     const updatePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
+        if (e.currentTarget.value.startsWith('-')) {
+            setErrorFlag(true);
+            setErrorMessage("Price must be at least $0");
+        } else {
+            setErrorFlag(false);
+        }
         setPrice(e.currentTarget.value);
     }
     const updateDescriptionState = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -308,7 +314,7 @@ const NewGame = () => {
                                         label="Price"
                                         onChange={updatePrice}
                                         value={price}
-                                        placeholder='0'
+                                        placeholder='Eg: 0 is $0, 999 is $9.99'
                                         // sx={{my: 2}}
                                     />
                                     </ListItemIcon>
@@ -332,7 +338,7 @@ const NewGame = () => {
                                             ))}
                                         </FormGroup>
                                     )}
-                                    {allPlatforms.filter(p=>p.isSelected === true).length < 1 && (
+                                    {platformChecked && (
                                         <FormLabel style={{color: "red"}} color='error'>Choose at least one platform</FormLabel>
                                     )}
                                 </Grid>
